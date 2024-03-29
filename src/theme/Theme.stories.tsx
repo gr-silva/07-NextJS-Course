848653af7627844421bf535f87656c17e06e1b58
Gabriel Rocha <gabriel@t2tsolucoes.com>
2024-03-28 21:57:53 -0300

    feat: add Theme documentation

---
+++ src/theme/Theme.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Theme from "./ThemeProvider";
+
+const meta: Meta<typeof Theme> = {
+  component: Theme,
+  title: "Intro/Theme",
+};
+
+export default meta;
+
+// type Story = StoryObj<typeof Theme>;
+
+// export const ButtonExample: Story = {
+//   args: {
+//     children: "Texto do Botão",
+//   },
+// };
