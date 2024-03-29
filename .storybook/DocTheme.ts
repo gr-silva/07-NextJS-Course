5d0d01b0cf93796e8c0652776b0aff06e48edc2d
Gabriel Rocha <gabriel@t2tsolucoes.com>
2024-03-28 22:14:15 -0300

    feat: add custom theme on Storybook

---
+++ .storybook/DocTheme.ts
@@ -0,0 +1,10 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "dark",
+  brandTitle: "T2T Soluções",
+  brandUrl: "www.t2tsolucoes.com",
+  brandImage:
+    "https://www.t2tsolucoes.com/wp-content/themes/t2tsolucoes/assets/images/t2t-logo-nav.svg",
+  brandTarget: "_self",
+});
